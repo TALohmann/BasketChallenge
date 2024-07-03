636f022e1f39c2b68d93f85137fd3f78db4f2dd7
Tim Lohmann <tim.a.lohmann@gmail.com>
2024-07-03 15:37:21 +0200

    initial maven project setup

---
+++ src/main/java/de/tabletopkiosk/basketchallenge/BasketchallengeApplication.java
@@ -0,0 +1,13 @@
+package de.tabletopkiosk.basketchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasketchallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasketchallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/tabletopkiosk/basketchallenge/BasketchallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package de.tabletopkiosk.basketchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasketchallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
