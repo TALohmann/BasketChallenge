7f457125d9b2c2c6f609cde6ccb99de0673ecce6
Tim Lohmann <tim.a.lohmann@gmail.com>
2024-07-03 18:40:01 +0200

    MOD removed unused dependencies and classes

--- src/main/java/de/tabletopkiosk/basketchallenge/model/BasketProduct.java
+++ src/main/java/de/tabletopkiosk/basketchallenge/model/BasketProduct.java
@@ -1,8 +1,5 @@
 package de.tabletopkiosk.basketchallenge.model;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,10 +7,6 @@ import lombok.Getter;
 @Builder
 @Getter
 public class BasketProduct implements IProduct {
-
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
 
 	@NotNull
 	private String barcode;
